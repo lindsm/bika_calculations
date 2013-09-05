3438e1c28947194f4b38c9c14bf71049666550e2
Mike Lindsay <lindsm@gmail.com>
2013-09-05 16:36:13 -0700

    First Draft

---
+++ total_d3.py
@@ -0,0 +1,14 @@
+from decimal import *
+
+def total_d3(round(d2,2),round(d3,),d2f,d3f):
+	"""Return the total value of D3 based on given criteria"""
+
+	#total_d3(5.3677,31.74,1.18,1.28) --> 29.0
+	print round(round(d2,2)/round(d2f,2),2)
+	print round(round(d3,2)/round(d3f,2),2)
+
+	if round(d2,2)
+
+	return round(round(d2,2)/round(d2f,2) + round(d3,2)/round(d3f,2),0)
+
+print total_d3(5.3677,31.74,1.18,1.28)
