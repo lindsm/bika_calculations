903548f2f827d09042fec3278bcc692b4fa11a1e
Mike Lindsay <lindsm@gmail.com>
2013-09-06 09:11:15 -0700

    Fixed return of D2 less than 1.96 to return D3 final, and not raw

--- total_d3.py
+++ total_d3.py
@@ -1,11 +1,11 @@
-def total_d3(d2,d3,d2f,d3f):
+def total_d3(d2_raw,d3_raw,d2_factor,d3_factor):
 	"""Return the total value of D3 based on given criteria"""
 	#example
 	#total_d3(5.3677,31.74,1.18,1.28) --> 29
 
-	if d2 < 1.96:  #If the D2 value is less than 1.96 ignore it and return the D3 value by itself as an integer.
-		return int(round(d3,0))
+	if d2_raw < 1.96:  #If the D2 value is less than 1.96 ignore it and return the D3 value by itself as an integer.
+		return int(round(d3_raw/d3_factor,0))
 	else:
-		return int(round(d2/d2f,2) + round(d3/d3f,2))
+		return int(round(d2_raw/d2_factor,2) + round(d3_raw/d3_factor,2))
 
 print total_d3(5.3677,31.74,1.18,1.28)
