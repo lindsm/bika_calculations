7439887d8a6ae6eb7e1c9c7dc17ac3b7039fa1b7
Mike Lindsay <lindsm@gmail.com>
2013-09-06 09:42:09 -0700

    Cleaned up DOC String

--- total_d3.py
+++ total_d3.py
@@ -1,7 +1,12 @@
 def total_d3(d2_raw,d3_raw,d2_factor,d3_factor):
-	"""Return the total value of D3 based on given criteria"""
-	#example
-	#total_d3(5.3677,31.74,1.18,1.28) --> 29
+	""" (number, number, number, number)  --> integer
+
+	Return the calculated integer value of d3_raw combined with d2_raw if d2_raw is greater than 1.96.
+
+
+	>>>total_d3(5.3677,31.74,1.18,1.28)
+	29	
+	"""
 
 	if d2_raw < 1.96:  #If the D2 value is less than 1.96 ignore it and return the D3 value by itself as an integer.
 		return int(round(d3_raw/d3_factor,0))
