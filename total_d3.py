ed5674eab5bce7c3383ce469bb97e0e05a1f005b
Mike Lindsay <lindsm@gmail.com>
2013-09-16 10:47:13 -0700

    Improved readability

--- total_d3.py
+++ total_d3.py
@@ -1,16 +1,16 @@
-def total_d3(d2_raw,d3_raw,d2_factor,d3_factor):
+def total_d3(d2_raw, d3_raw, d2_factor, d3_factor):
 	""" (number, number, number, number)  --> integer
 
-	Return the calculated integer value of d3_raw. If d2_raw is greater than 1.95 add it to the returned value.
+	Return the integer value of d3_raw divided by d3_factor plus d2_raw divided by d2_factor if d2_raw is greater than 1.95.  If d2_raw is less than 1.96 then return d3_raw divided by d3_factor.
 
 
-	>>>total_d3(5.3677,31.74,1.18,1.28)
+	>>>total_d3(5.3677, 31.74, 1.18, 1.28)
 	29	
 	"""
 
-	if d2_raw < 1.96:  #If the D2 value is less than 1.96 ignore it and return the D3 value by itself as an integer.
-		return int(round(d3_raw/d3_factor,0))
+	if d2_raw > 1.95:  #If the D2 value is less than 1.96 ignore it and return the D3 value by itself as an integer.
+		return int(round(d2_raw / d2_factor, 2) + round(d3_raw / d3_factor, 2))
 	else:
-		return int(round(d2_raw/d2_factor,2) + round(d3_raw/d3_factor,2))
+		return int(round(d3_raw / d3_factor, 0))
 
-print total_d3(5.3677,31.74,1.18,1.28)
+print total_d3(5.3677, 31.74, 1.18, 1.28)
